caccd92f316f4080e526d9a6efc8f70abd57373d
yo0919 <104425318+yo0919@users.noreply.github.com>
2024-01-17 17:14:13 +0900

    refactor: HTTP 요청 및 응답 처리 로직 개선 (#9)
    - HttpRequest 클래스 추가: HTTP 요청의 세부사항을 파싱하고 저장하는 기능 구현
    - HttpResponse 클래스 추가: HTTP 응답 구성 및 관리 기능 구현
    - UserController 수정: HttpRequest 및 HttpResponse를 사용하여 회원가입 처리법 변경 및 회원가입 완료시 홈페이지로 리디렉션
    - UserUtils 수정: 요청 데이터를 기반으로 User 객체 생성하는 로직 개선
    - RequestHandler 수정: HttpRequest와 HttpResponse를 사용하여 요청 처리 로직 개선 및 정적 파일 처리 로직 개선

---
+++ src/main/java/http/HttpResponse.java
@@ -0,0 +1,76 @@
+package http;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HttpResponse {
+    private static final Logger logger = LoggerFactory.getLogger(HttpResponse.class);
+    private int statusCode;
+    private Map<String, String> headers;
+    private byte[] body;
+
+    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+        try {
+            dos.writeBytes("HTTP/1.1 200 OK \r\n");
+            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+    private void responseBody(DataOutputStream dos, byte[] body) {
+        try {
+            dos.write(body, 0, body.length);
+            dos.flush();
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+    private void response404Header(DataOutputStream dos) {
+        try {
+            dos.writeBytes("HTTP/1.1 404 Not Found\r\n");
+            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+    public HttpResponse() {
+        this.headers = new HashMap<>();
+    }
+
+    public void setStatusCode(int statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public void setHeader(String key, String value) {
+        headers.put(key, value);
+    }
+
+    public void setBody(byte[] body) {
+        this.body = body;
+        if (body != null) {
+            this.headers.put("Content-Length", String.valueOf(body.length));
+        }
+    }
+
+    public void send(DataOutputStream dos) throws IOException {
+        dos.writeBytes("HTTP/1.1 " + statusCode + " \r\n");
+        for (Map.Entry<String, String> header : headers.entrySet()) {
+            dos.writeBytes(header.getKey() + ": " + header.getValue() + "\r\n");
+        }
+        dos.writeBytes("\r\n");
+        if (body != null) {
+            dos.write(body, 0, body.length);
+        }
+        dos.flush();
+    }
+}

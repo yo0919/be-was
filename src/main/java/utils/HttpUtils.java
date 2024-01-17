1f74cd8276c554fbf098acb2d6350547fcc26442
yo0919 <104425318+yo0919@users.noreply.github.com>
2024-01-17 17:22:15 +0900

    refactor: 불필요한 HttpUtils 클래스 제거 (#10)
    - 프로젝트 내에서 HttpUtils.extractUrl 메서드가 HttpRequest클래스로 대체됨
    - 더 이상 사용되지 않는 HttpUtils 클래스를 삭제하여 코드 정리 및 유지 관리 용이성 향상

--- src/main/java/utils/HttpUtils.java
+++
@@ -1,9 +0,0 @@
-package utils;
-
-public class HttpUtils {
-
-    public static String extractUrl(String httpRequest) {
-        String requestLine = httpRequest.split("\n")[0];
-        return requestLine.split(" ")[1];
-    }
-}

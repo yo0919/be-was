1e5898a45e7058389c4ae7c968013bd9e941af8b
yo0919 <0919yo@naver.com>
2024-01-18 16:35:46 +0900

    refactor : readHttpRequest 메서드 클래스 분리
    - readHttpRequest 메서드를 HttpUtils 클래스 안으로 분리

---
+++ src/main/java/utils/HttpUtils.java
@@ -0,0 +1,19 @@
+// http 패키지 내의 HttpUtils 클래스
+package http;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class HttpUtils {
+    public static String readHttpRequest(InputStream in) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+        StringBuilder requestBuilder = new StringBuilder();
+        String line;
+        while ((line = br.readLine()) != null && !line.isEmpty()) {
+            requestBuilder.append(line).append("\n");
+        }
+        return requestBuilder.toString();
+    }
+}

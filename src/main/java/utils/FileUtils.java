ce4ab965de975e76449fe7e8beae5b5e668fa6f3
yo0919 <0919yo@naver.com>
2024-01-23 10:54:29 +0900

    refactor : FileUtils 의존성 변경
    - 기존 nio를 사용하던 방식에서 io를 사용하도록 변경

--- src/main/java/utils/FileUtils.java
+++ src/main/java/utils/FileUtils.java
@@ -1,20 +1,21 @@
 package utils;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.BufferedInputStream;
 
 public class FileUtils {
 
     public static byte[] readFile(String filePath) throws IOException {
-        Path path = Paths.get(filePath).normalize();
-        if (Files.exists(path)) {
-            return Files.readAllBytes(path);
+        File file = new File(filePath);
+        if (file.exists() && file.isFile()) {
+            try (BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file))) {
+                byte[] data = new byte[(int) file.length()];
+                bis.read(data);
+                return data;
+            }
         }
         return null;
-    }
-    public static void writeFile(String filePath, String content) throws IOException {
-        Files.write(Paths.get(filePath), content.getBytes());
     }
 }

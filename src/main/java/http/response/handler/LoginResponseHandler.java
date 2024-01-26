8e2e166094d9b0e80fc2abfd946aff4be3dcb63f
yo0919 <0919yo@naver.com>
2024-01-26 10:53:08 +0900

    chore : LoginResponseHandler에 로그추가하여 진행상황확인

--- src/main/java/http/response/handler/LoginResponseHandler.java
+++ src/main/java/http/response/handler/LoginResponseHandler.java
@@ -5,10 +5,13 @@ import http.response.HttpResponse;
 import http.session.SessionHandler;
 import model.User;
 import http.response.renderer.HtmlResponseRenderer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class LoginResponseHandler {
 
     private final SessionHandler sessionHandler;
+    private static final Logger logger = LoggerFactory.getLogger(LoginResponseHandler.class);
 
     public LoginResponseHandler() {
         this.sessionHandler = new SessionHandler();
@@ -16,10 +19,15 @@ public class LoginResponseHandler {
 
     public void handle(HttpRequest request, HttpResponse response) {
         User loggedInUser = sessionHandler.checkSession(request);
+        logger.debug("Handling login response for request path: {}", request.getPath());
+        logger.debug("Logged in user: {}", loggedInUser);
+
         if (request.getPath().equals("/index.html") || request.getPath().equals("/")) {
             byte[] responseBody = HtmlResponseRenderer.renderWelcomePage(loggedInUser);
             response.setBody(responseBody);
             response.setHeader("Content-Type", "text/html");
+            logger.debug("Response body set for welcome page");
         }
     }
 }
+

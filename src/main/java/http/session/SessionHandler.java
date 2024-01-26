449be1f8f784bc62ac7666e1e1d34611c5dcc617
yo0919 <0919yo@naver.com>
2024-01-26 10:44:18 +0900

    chore : SessionHandler에 로그추가하여 진행상황확인

--- src/main/java/http/session/SessionHandler.java
+++ src/main/java/http/session/SessionHandler.java
@@ -2,13 +2,23 @@ package http.session;
 
 import model.User;
 import http.request.HttpRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SessionHandler {
+    private static final Logger logger = LoggerFactory.getLogger(SessionHandler.class);
+
     public User checkSession(HttpRequest request) {
         String sessionId = request.getSessionId();
+        logger.debug("Checking session with ID: {}", sessionId);
+
         if (sessionId != null) {
-            return SessionStorage.getInstance().getUserBySessionId(sessionId);
+            User user = SessionStorage.getInstance().getUserBySessionId(sessionId);
+            logger.debug("User retrieved from session: {}", user);
+            return user;
         }
+
+        logger.debug("No session ID found or no user associated with this session ID.");
         return null;
     }
 }

37c27931ea4c09627e4cf1ebbc978bf4c701dc63
yo0919 <0919yo@naver.com>
2024-01-29 12:11:36 +0900

    feat : 환영문구 위치수정
    - styles.css 수정하여 환영문구의 높이 수정

--- src/main/java/http/response/renderer/HtmlResponseRenderer.java
+++ src/main/java/http/response/renderer/HtmlResponseRenderer.java
@@ -9,20 +9,21 @@ public class HtmlResponseRenderer {
         try {
             String htmlContent = FileUtils.readFileAsString(filePath);
 
-            // 로그인과 회원가입 버튼을 포함하는 HTML 부분을 찾아서 사용자 인사말로 교체
-            String userGreeting = "<li class=\"nav-item\">안녕하세요, " + userName + "님!</li>";
+            // 환영 문구를 먼저 추가
+            String userGreeting = "<li class=\"nav-item welcome-message\">안녕하세요, 전용호님!</li>\n";
 
-            // 기존 로그인/회원가입 버튼을 찾아 교체
+            // "Posts" 버튼과 환영 문구의 순서를 변경
             htmlContent = htmlContent.replaceFirst(
                     "<ul class=\"nav navbar-nav navbar-right\">\\s*<li class=\"active\"><a href=\"index.html\">Posts</a></li>",
                     "<ul class=\"nav navbar-nav navbar-right\">" +
-                            "<li class=\"active\"><a href=\"index.html\">Posts</a></li>" + userGreeting
+                            userGreeting + "<li class=\"active\"><a href=\"index.html\">Posts</a></li>"
             );
 
             // 로그인/회원가입 버튼을 삭제
             htmlContent = htmlContent.replaceAll(
                             "<li><a href=\"user/login.html\" role=\"button\">로그인</a></li>", "")
                     .replaceAll("<li><a href=\"user/form.html\" role=\"button\">회원가입</a></li>", "");
+
 
             // 로그 출력
             logger.debug("Transformed HTML: {}", htmlContent);
